6e37c395f1311933da3ff0ab77975bc0780f7108
nina <juan.amado.sicat.cleto@gmail.com>
2025-07-14 20:16:09 +0800

    Bro

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ void enqueue(int arr[], int data, int &rear, int &front) {
     }
 }
 
-void pop(int arr[], int data, int &rear, int &front){
+void pop(int arr[], int &rear, int &front){
 
     if (rear == -1) {
         cout << "Queue is empty" << endl;
@@ -32,8 +32,11 @@ void pop(int arr[], int data, int &rear, int &front){
 
 }
 
-void examineElement(int arr[], int data, int rear, int front) {
+void destroyArray(int &front, int &rear) {
+    front = -1;
+    rear = -1;
 
+    cout << "Array destroyed" << endl;
 }
 
 void printArray(int arr[], int rear, int front) {
@@ -41,10 +44,16 @@ void printArray(int arr[], int rear, int front) {
         cout << "List is empty!" << endl;
     }
     else {
+        cout << "{";
         for (int i = front; i <= rear; i++) {
-            cout << arr[i] << " ";
+            if (i != rear)
+                cout << arr[i] << ",";
+            else
+                cout << arr[i];
         }
+        cout << "}" << endl;
     }
+    cout << endl;
 }
 
 void runProgram() {
@@ -54,12 +63,13 @@ void runProgram() {
     int data;
     int input;
 
-    while (input != 4) {
+    while (input != 5) {
 
         cout << "1. Enqueue" << endl;
-        cout << "2. Dequeue" << endl;
+        cout << "2. Pop" << endl;
         cout << "3. Print Array" << endl;
-        cout << "4. Exit" << endl;
+        cout << "4. Reset" << endl;
+        cout << "5. Exit" << endl;
         cout << ">>>> ";
         cin >> input;
 
@@ -70,14 +80,15 @@ void runProgram() {
                 enqueue(queue, data, rear, front);
                 break;
             case 2:
-                cout << "Enter the data: ";
-                cin >> data;
-                pop(queue, data, rear, front);
+                pop(queue, rear, front);
                 break;
             case 3:
                 printArray(queue, rear, front);
                 break;
             case 4:
+                destroyArray(front, rear);
+                break;
+            case 5:
                 cout << "Exiting..." << endl;
                 break;
             default:
